6906d0cac762d10acec52dba10ed230e38e8917c
Delisa Mason <iskanamagus@gmail.com>
2013-06-27 10:11:47 -0400

    Initial Commit

---
+++ Attica/Attica.h
@@ -0,0 +1,13 @@
+//
+//  Attica.h
+//  Attica
+//
+//  Created by Delisa Mason on 6/27/13.
+//  Copyright (c) 2013 Delisa Mason. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface Attica : NSObject
+
+@end
